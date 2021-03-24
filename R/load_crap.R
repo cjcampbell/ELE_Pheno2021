2365758b0811b5d399ad8fb4d50ba719d6ef34f8
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2021-03-23 23:39:50 -0400

    tidy correctly

--- R/load_crap.R
+++ R/load_crap.R
@@ -1,15 +1,26 @@
 
 # Load Fric et al reanalyses ---------------------------------------------
 library(XLConnect)
+library(tidyverse)
 tmp = tempfile(fileext = ".xlsx")
+
 download.file("https://onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1111%2Fele.13739&file=ele13739-sup-0002-TableS2.xlsx",  destfile = tmp, mode="wb")
 
 # Load the fourth ("bottom") set of rows, corresponding to the more-heavily filtered data used to compare to the LS letter:
 fricData <- readWorksheetFromFile(file = tmp, sheet = "~latitude|altitude+year", header = TRUE, startRow = 346, endRow = 434)
 
-names(fricData) <- gsub(".1", "_peak", gsub(".2", "_onset", gsub(".3", "_term", names(mdf)) ))
+# Rename columns to reflect peak, onset, and termination coefs.
+names(fricData)[2:9] <- paste0(names(fricData)[2:9], "_peak")
+names(fricData)[10:17] <- paste0(gsub(".1", "", names(fricData)[10:17]), "_onset")
+names(fricData)[18:25] <- paste0(gsub(".2", "", names(fricData)[18:25]), "_term")
 
-write.csv(file.path("Fric_data", "fricData.csv"))
+# Remove extraneous detail from name column:
+fricData<- fricData %>% 
+  dplyr::mutate(verbatimName = Name,
+         Name = sub("^(\\S*\\s+\\S+).*", "\\1", Name)) %>% 
+  dplyr::rename(name = Name)
+
+saveRDS(fricData, file.path("data", "fricData.rds"))
 
 # Run LS analyses ---------------------------------------------------------
 
